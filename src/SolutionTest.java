1fc293e05840e72a51d3a61b7e83b3e3746d70e6
unknown <lbvbxlbvbx@gmail.com>
2021-08-04 16:41:59 +0300

    added tests and formated code

--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SolutionTest {
+
+    @Test
+    void testSolution() throws Exception {
+        int [] nums ={1,2,3,1,1,3};
+        int [] nums1 ={1,1,1,1};
+        int [] nums2 ={1,2,3};
+        assertEquals(4,Solution.numIdenticalPairs(nums));
+        assertEquals(6,Solution.numIdenticalPairs(nums1));
+        assertEquals(0,Solution.numIdenticalPairs(nums2));
+    }
 }
